b5cdd29bfa21002131a2091dabbf119a37c5084b
Galuh Utama <galuh.utama@meondi.com>
2024-05-30 03:50:39 +0200

    Simplified code using SDL2 gfx

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL2_gfxPrimitives.h>
 #include <linux/can.h>
 #include <linux/can/raw.h>
 #include <net/if.h>
@@ -71,8 +72,6 @@ bool init() {
         return false;
     }
 
-    SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
-
     can_socket = socket(PF_CAN, SOCK_RAW, CAN_RAW);
     if (can_socket < 0) {
         std::cerr << "Error while opening socket" << std::endl;
@@ -116,111 +115,34 @@ void draw_text(const std::string &text, int x, int y, TTF_Font *font, SDL_Color 
     SDL_DestroyTexture(texture);
 }
 
-void draw_circle_primitive(SDL_Renderer *renderer, int32_t centreX, int32_t centreY, int32_t radius) {
-    const int32_t diameter = (radius * 2);
-
-    int32_t x = (radius - 1);
-    int32_t y = 0;
-    int32_t tx = 1;
-    int32_t ty = 1;
-    int32_t error = (tx - diameter);
-
-    while (x >= y) {
-        //  Each of the following renders an octant of the circle
-        SDL_RenderDrawPoint(renderer, centreX + x, centreY - y);
-        SDL_RenderDrawPoint(renderer, centreX + x, centreY + y);
-        SDL_RenderDrawPoint(renderer, centreX - x, centreY - y);
-        SDL_RenderDrawPoint(renderer, centreX - x, centreY + y);
-        SDL_RenderDrawPoint(renderer, centreX + y, centreY - x);
-        SDL_RenderDrawPoint(renderer, centreX + y, centreY + x);
-        SDL_RenderDrawPoint(renderer, centreX - y, centreY - x);
-        SDL_RenderDrawPoint(renderer, centreX - y, centreY + x);
-
-        if (error <= 0) {
-            ++y;
-            error += ty;
-            ty += 2;
-        }
-
-        if (error > 0) {
-            --x;
-            tx += 2;
-            error += (tx - diameter);
-        }
-    }
-}
-
-void fill_circle(SDL_Renderer *surface, int cx, int cy, int radius, SDL_Color color) {
-    SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, 255);
-    for (int i = 1; i < radius; i++) {
-        int r_fake = i;
-        draw_circle_primitive(surface, cx, cy, r_fake);	
-    }
-}
-
 void draw_circle_glow(int center_x, int center_y, int inner_radius, int outer_radius, SDL_Color color) {
     for (int i = 0; i < (outer_radius - inner_radius); i++) {
         int alpha = static_cast<int>(255 * (1 - i / static_cast<float>(outer_radius - inner_radius)));
-        SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, alpha);
-        draw_circle_primitive(renderer, center_x, center_y, inner_radius + i);
-    }
-}
-
-void draw_line(SDL_Renderer *surface, int x1, int y1, int x2, int y2, SDL_Color color, int width) {
-    SDL_SetRenderDrawColor(surface, color.r, color.g, color.b, 255);
-
-    // Calculate the direction of the line
-    double dx = x2 - x1;
-    double dy = y2 - y1;
-    double length = sqrt(dx * dx + dy * dy);
-
-    // Calculate the unit vector perpendicular to the line
-    double ux = -dy / length;
-    double uy = dx / length;
-
-    // Loop to draw multiple parallel lines for the thickness
-    for (int i = -width / 2; i <= width / 2; ++i) {
-        double offset_x = ux * i;
-        double offset_y = uy * i;
-        SDL_RenderDrawLine(surface, x1 + offset_x, y1 + offset_y, x2 + offset_x, y2 + offset_y);
-    }
-}
-
-void draw_circle(int center_x, int center_y, int radius, SDL_Color color, SDL_Color border_color, int border_width,
-                 bool fill) {
-    // Set color for the circle fill
-    SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, 255);
-
-    // Draw filled circle
-    draw_circle_primitive(renderer, center_x, center_y, radius);
-
-    if (fill) {
-        fill_circle(renderer, center_x, center_y, radius, color);
-    }
-
-    // Draw the border
-    if (border_width > 0) {
-        SDL_SetRenderDrawColor(renderer, border_color.r, border_color.g, border_color.b, 255);
-        draw_circle_primitive(renderer, center_x, center_y, radius - border_width);
+        aacircleRGBA(renderer, center_x, center_y, inner_radius + i, color.r, color.g, color.b, alpha);
     }
 }
 
 void
 draw_gauge(int center_x, int center_y, int value, int max_value, int tick_min_value, int tick_max_value, int tick_step,
            const std::string &label) {
+    // outer ring
     draw_circle_glow(center_x, center_y, RADIUS, RADIUS + 20, BLUE_PURPLE);
 
+    // needle
     float angle = static_cast<float>(value) / max_value * 270 - 225;
     int x = center_x + (RADIUS - 95) * cos(angle * M_PI / 180.0);
     int y = center_y + (RADIUS - 95) * sin(angle * M_PI / 180.0);
     int x2 = center_x + RADIUS * cos(angle * M_PI / 180.0);
     int y2 = center_y + RADIUS * sin(angle * M_PI / 180.0);
-    draw_line(renderer, x, y, x2, y2, BLUE_PURPLE, 8);
+    thickLineRGBA(renderer, x, y, x2, y2, 8, BLUE_PURPLE.r, BLUE_PURPLE.g, BLUE_PURPLE.b, 254);
 
-    draw_circle(center_x, center_y, 130, BLACK, BLUE_PURPLE, 1, false); 
+    // inner ring
+    aacircleRGBA(renderer, center_x, center_y, 130, BLUE_PURPLE.r, BLUE_PURPLE.g, BLUE_PURPLE.b, 254);
     draw_circle_glow(center_x, center_y, 130, 180, DARK_PURPLE);
 
+    // ticks
     for (int i = tick_min_value; i <= tick_max_value; i += tick_step) {
+        // main ticks
         SDL_Color tick_color = i >= tick_max_value - 2 * tick_step ? RED_ORANGE : LIGHT_GRAY;
         SDL_Color in_between_tick_color = i > tick_max_value - 3 * tick_step ? DARK_RED_ORANGE : DARK_GRAY;
 
@@ -229,8 +151,9 @@ draw_gauge(int center_x, int center_y, int value, int max_value, int tick_min_va
         int y1 = center_y + (RADIUS - 30 * 0.8) * sin(angle * M_PI / 180.0);
         int x2 = center_x + RADIUS * cos(angle * M_PI / 180.0);
         int y2 = center_y + RADIUS * sin(angle * M_PI / 180.0);
-	draw_line(renderer, x1, y1, x2, y2, tick_color, 1);
+        aalineRGBA(renderer, x1, y1, x2, y2, tick_color.r, tick_color.g, tick_color.b, 254);
 
+        // inbetween ticks
         if (i < tick_max_value) {
             float in_between_angle =
                     static_cast<float>(i + tick_step / 2 - tick_min_value) / (tick_max_value - tick_min_value) * 270 - 225;
@@ -238,15 +161,17 @@ draw_gauge(int center_x, int center_y, int value, int max_value, int tick_min_va
             int in_between_y1 = center_y + (RADIUS - 20 * 0.8) * sin(in_between_angle * M_PI / 180.0);
             int in_between_x2 = center_x + RADIUS * cos(in_between_angle * M_PI / 180.0);
             int in_between_y2 = center_y + RADIUS * sin(in_between_angle * M_PI / 180.0);
-	    draw_line(renderer, in_between_x1, in_between_y1, in_between_x2, in_between_y2, in_between_tick_color, 1);
+            aalineRGBA(renderer, in_between_x1, in_between_y1, in_between_x2, in_between_y2, in_between_tick_color.r, in_between_tick_color.g, in_between_tick_color.b, 254);
         }
 
+        // tick label
         float text_angle = static_cast<float>(i - tick_min_value) / (tick_max_value - tick_min_value) * 270 - 225;
         int text_x = center_x + (RADIUS - 60) * cos(text_angle * M_PI / 180.0);
         int text_y = center_y + (RADIUS - 60) * sin(text_angle * M_PI / 180.0);
         draw_text(std::to_string(i), text_x - 20, text_y - 10, small_font, DARK_GRAY);
     }
 
+    // value
     int text_width = 0;
     TTF_SizeText(large_font, std::to_string(value).c_str(), &text_width, nullptr);
     draw_text(std::to_string(value), center_x - text_width / 2, center_y - 35, large_font, LIGHT_GRAY);
@@ -274,7 +199,7 @@ void main_loop() {
     }
 }
 
-void close() {
+void destroy() {
     TTF_CloseFont(large_font);
     TTF_CloseFont(normal_font);
     TTF_CloseFont(small_font);
@@ -282,7 +207,7 @@ void close() {
     SDL_DestroyWindow(window);
     TTF_Quit();
     SDL_Quit();
-    ::close(can_socket);
+    close(can_socket);
 }
 
 int main(int argc, char *argv[]) {
@@ -296,7 +221,7 @@ int main(int argc, char *argv[]) {
 
     running = false;
     can_thread.join();
-    close();
+    destroy();
 
     return 0;
 }
